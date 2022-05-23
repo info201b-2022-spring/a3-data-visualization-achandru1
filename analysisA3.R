e5acbd4d447c07105ea81f0e26404803336ba3b3
achandru1 <abic@uw.edu>
2022-05-22 19:50:53 -0700

    FINAL

--- analysisA3.R
+++ analysisA3.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(maps)
-library(mapproj)
 
 incarceration_trends <- read.csv("https://raw.githubusercontent.com/vera-institute/incarceration-trends/master/incarceration_trends.csv")
 two_decades_incarceration <- incarceration_trends[incarceration_trends$year >= 1995 & incarceration_trends$year <= 2015, ] %>%
@@ -17,14 +16,16 @@ two_decades_incarceration <- incarceration_trends[incarceration_trends$year >= 1
   mutate(latinx_prop = total_latinx_pop / total_prison_jail_pop) %>%
   mutate(native_prop = total_native_pop / total_prison_jail_pop) %>%
   mutate(white_prop = total_white_pop / total_prison_jail_pop)
-  
+
 #Summary Information
 #1. What is the total jail + prison population for minorities (AAPI, African American, Latinx, Native Americans) in the US since 1995?
 total_minority_pop_num <- sum(two_decades_incarceration$total_aapi_pop, na.rm = TRUE) + sum(two_decades_incarceration$total_black_pop, na.rm = TRUE) +
   sum(two_decades_incarceration$total_latinx_pop, na.rm = TRUE) + sum(two_decades_incarceration$total_native_pop, na.rm = TRUE)
+round(total_minority_pop_num, digits = 0)
 
 #2. What is the total jail + prison population for Whites in the US since 1995?
 total_white_pop_num <- sum(two_decades_incarceration$total_white_pop, na.rm = TRUE)
+round(total_white_pop_num, digits = 0)
 
 #3. What is the proportion of minorities in jails and prisons combined?
 minority_prop_num <- total_minority_pop_num / sum(two_decades_incarceration$total_prison_jail_pop, na.rm = TRUE)
@@ -32,7 +33,7 @@ minority_prop_num <- total_minority_pop_num / sum(two_decades_incarceration$tota
 #4. What is the proportion of Whites in jails and prisons combined?
 white_prop_num <- total_white_pop_num / sum(two_decades_incarceration$total_prison_jail_pop, na.rm = TRUE)
 
-#5. In what region of the US was the proportion of minorities in jails and prisons combined highest?
+#5. In what region(s) of the US was the proportion of minorities in jails and prisons combined highest and lowest?
 region_based <- two_decades_incarceration %>%
   group_by(region) %>%
   summarise(total_aapi_pop_jp = sum(total_aapi_pop, na.rm = TRUE), 
@@ -44,28 +45,30 @@ region_based <- two_decades_incarceration %>%
   mutate(total_minority_pop_jp = total_aapi_pop_jp + total_black_pop_jp + total_latinx_pop_jp + total_native_pop_jp) %>%
   mutate(minority_prop = total_minority_pop_jp / total_jp_pop)
 highest_minority_prop_region <- toString(region_based[region_based$minority_prop == max(region_based$minority_prop), "region"])
+lowest_minority_prop_region <- toString(region_based[region_based$minority_prop == min(region_based$minority_prop), "region"])
 
-#6. In what region of the US was the proportion of Whites in jails and prisons combined highest?
+#6. In what region(s) of the US was the proportion of Whites in jails and prisons combined highest and lowest?
 region_based <- region_based %>%
   mutate(white_prop = total_white_pop_jp / total_jp_pop)
 highest_white_prop_region <- toString(region_based[region_based$white_prop == max(region_based$white_prop), "region"])
+lowest_white_prop_region <- toString(region_based[region_based$white_prop == min(region_based$white_prop), "region"])
 
 #Trend Chart
 race_pop_by_year <- two_decades_incarceration %>%
   group_by(year) %>%
-  summarise(total_aapi_pop_jp = sum(total_aapi_pop, na.rm = TRUE),
-            total_black_pop_jp = sum(total_black_pop, na.rm = TRUE),
-            total_latinx_pop_jp = sum(total_latinx_pop, na.rm = TRUE),
-            total_native_pop_jp = sum(total_native_pop, na.rm = TRUE),
-            total_white_pop_jp = sum(total_white_pop, na.rm = TRUE))
+  summarise(total_aapi_pop = sum(total_aapi_pop, na.rm = TRUE),
+            total_black_pop = sum(total_black_pop, na.rm = TRUE),
+            total_latinx_pop = sum(total_latinx_pop, na.rm = TRUE),
+            total_native_pop = sum(total_native_pop, na.rm = TRUE),
+            total_white_pop = sum(total_white_pop, na.rm = TRUE))
 
 options(scipen = 1000000)
 trend_chart <- ggplot(data = race_pop_by_year, aes(x = year, color = group)) +
-  geom_line(aes(y = total_aapi_pop_tc, color = "red")) + 
-  geom_line(aes(y = total_black_pop_tc, color = "yellow")) + 
-  geom_line(aes(y = total_latinx_pop_tc, color = "purple")) + 
-  geom_line(aes(y = total_native_pop_tc, color = "green")) + 
-  geom_line(aes(y = total_white_pop_tc, color = "blue")) +
+  geom_line(aes(y = total_aapi_pop, color = "red")) + 
+  geom_line(aes(y = total_black_pop, color = "yellow")) + 
+  geom_line(aes(y = total_latinx_pop, color = "purple")) + 
+  geom_line(aes(y = total_native_pop, color = "green")) + 
+  geom_line(aes(y = total_white_pop, color = "blue")) +
   labs(x = "Year", y = "Total Prison + Jail Population") +
   scale_color_manual(name = "Race", labels = c("White", "Native", "Latinx", "AAPI", "Black"), 
                      values = c("blue", "green", "purple", "red", "black")) +
@@ -85,30 +88,39 @@ var_com_chart <- ggplot(data = white_vs_black_prop[which(white_vs_black_prop$bla
 
 #Map
 only_2015 <- two_decades_incarceration %>%
-  filter(year == 2015) %>%
-  rename(county = county_name)
+  rename(county = county_name) %>%
+  select(fips, black_prop, year)
+
+#this code chunk (and the one below) was taken from stackoverflow because merging the datasets with the normal method 
+#was causing my data values to become NA. 
+fips_df <- maps::county.fips %>%
+  as.tibble() %>%
+  extract(polyname, c("region", "subregion"), "^([^,]+),([^,]+)$")
 
 county_shape <- map_data("county") %>%
-  rename(county = region) %>%
-  left_join(only_2015, by = "county")
+  left_join(fips_df)
+
+only_2015 <- only_2015 %>%
+  left_join(county_shape, by = "fips") %>%
+  filter(year == 2015)
 
 map <- ggplot(county_shape) +
-  geom_polygon(
-    mapping = aes(x = long, y = lat, group = group, fill = black_prop),
-    color = "white", # show state outlines
-    size = .1        # thinly stroked
+  geom_polygon(data = only_2015,
+               mapping = aes(x = long, y = lat, group = group, fill = black_prop),
+               color = "white",
+               size = .1
   ) +
   coord_map() + # use a map-based coordinate system
-  scale_fill_continuous(low = "#132B43", high = "Red") +
-  labs(fill = "Proportion of Blacks in Prisons + Jails in 2015")
-  
- 
-  
+  scale_fill_viridis_c(option = "magma", direction = -1) +
+  labs(fill = "Proportion of Blacks in Prisons + Jails in 2015") +
+  theme(
+    axis.line = element_blank(),        # remove axis lines
+    axis.text = element_blank(),        # remove axis labels
+    axis.ticks = element_blank(),       # remove axis ticks
+    axis.title = element_blank(),       # remove axis titles
+    plot.background = element_blank(),  # remove gray background
+    panel.grid.major = element_blank(), # remove major grid lines
+    panel.grid.minor = element_blank(), # remove minor grid lines
+    panel.border = element_blank()      # remove border around plot
+  )
 
-
-
-
-
-
-
-county_fips <- cl;kj
